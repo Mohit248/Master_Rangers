29608bafcdf7e0f999076901504d91e67f1ad049
Mohit248 <83233932+Mohit248@users.noreply.github.com>
2021-08-30 21:44:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,8 +13,8 @@ function new_image(get_image)
 	fabric.Image.fromURL(get_image, function(Img) {
     block_image_object = Img;
 
-	block_image_object.scaleToWidth(block_image_object);
-	block_image_object.scaleToHeight(block_image_object);
+	block_image_object.scaleToWidth(400);
+	block_image_object.scaleToHeight(390);
 	block_image_object.set({
     top:block_y,
 	left:block_x
@@ -33,13 +33,13 @@ console.log(keyPressed);
 	if(keyPressed == '82') 
 	{
 		new_image("rr1.png");
-		console.log("R");
+		console.log("r");
 	}
 	if(keyPressed == '71')
 	{
 		block_x = 200;
 		new_image("gr.png");
-		console.log("G");
+		console.log("g");
 		
 	}
 	
@@ -47,21 +47,21 @@ console.log(keyPressed);
 	{
 		block_x =350;
 		new_image("yr.png");
-		console.log("Y");
+		console.log("y");
 		
 	}
 	if(keyPressed == '80')
 	{
 		block_x = 600;
 		new_image("pr.png");
-		console.log("P");
+		console.log("p");
 		
 	}
 	if(keyPressed == '66')
 	{
 		block_x = 700;
 	    new_image("br.png");
-		console.log("B");
+		console.log("b");
 		
 	}
 	
